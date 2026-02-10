9514b3442e01d1b69888949dc41f8728d95229a0
flat-it.corp <53145040+hiroyuki-miyaji@users.noreply.github.com>
2026-02-10 16:52:01 +0900

    Create env.prod.js

---
+++ js/env.prod.js
@@ -0,0 +1,9 @@
+/****************************************************
+ * 本番環境設定（本番）
+ ****************************************************/
+export const ENV = {
+  ENV_NAME: "PROD",
+  LIFF_ID: "2008634162-jVqAPKrD",
+  API_URL:
+    "https://prod-13.japaneast.logic.azure.com:443/workflows/7d86cca357d74a499d659ccfddac499c/triggers/When_an_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_an_HTTP_request_is_received%2Frun&sv=1.0&sig=qalRj8hNDNVdcAXhZ7cpC6KahERkg5W3NcBcPseEl14"
+};
