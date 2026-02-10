0a5a898dcf9fec5b682c8a0dea04eed644e497fe
flat-it.corp <53145040+hiroyuki-miyaji@users.noreply.github.com>
2026-02-10 23:18:21 +0900

    Update app.js

--- js/app.js
+++ js/app.js
@@ -13,8 +13,8 @@ if (location.search.includes("reset=1")) {
   alert("localStorage cleared");
 }
 
-LIFF_ID= "2008634162-jVqAPKrD";
-API_URL=
+const LIFF_ID= "2008634162-jVqAPKrD";
+const API_URL=
   "https://prod-13.japaneast.logic.azure.com:443/workflows/7d86cca357d74a499d659ccfddac499c/triggers/When_an_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_an_HTTP_request_is_received%2Frun&sv=1.0&sig=qalRj8hNDNVdcAXhZ7cpC6KahERkg5W3NcBcPseEl14";
 
 //const LIFF_ID = ENV.LIFF_ID;
