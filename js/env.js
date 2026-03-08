116e82b612987e05ae14c2431a748b5fffc6db45
flat-it.corp <miyaji.h2010@gmail.com>
2026-03-08 23:41:46 +0900

    Update env.js

--- js/env.js
+++ js/env.js
@@ -22,6 +22,6 @@
   window.ENV = {
     ENV_NAME: "prod",
     LIFF_ID: "2008634162-jVqAPKrD",
-    API_URL: "https://prod-13.japaneast.logic.azure.com:443/workflows/7d86cca357d74a499d659ccfddac499c/triggers/When_an_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_an_HTTP_request_is_received%2Frun&sv=1.0&sig=qalRj8hNDNVdcAXhZ7cpC6KahERkg5W3NcBcPseEl14"
+    API_URL: "https://prod-18.japaneast.logic.azure.com:443/workflows/07cde59b24224e6b9b5ceab6188bf89f/triggers/When_an_HTTP_request_is_received/paths/invoke?api-version=2016-10-01&sp=%2Ftriggers%2FWhen_an_HTTP_request_is_received%2Frun&sv=1.0&sig=RAH4eYJqOLGzLCumaE2gXq-LzdRb8JBb-EyeSATGvcg"
   };
 })();
